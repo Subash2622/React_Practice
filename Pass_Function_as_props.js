65b92f3e123c02b0645a1d6a294f394d15d71b25
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-25 12:37:52 +0530

    Update Pass_Function_as_props.js

--- Pass_Function_as_props.js
+++ Pass_Function_as_props.js
@@ -1,11 +1,45 @@
-import "./App.css";
-import Profile from "./Profile";
+import logo from './logo.svg';
+import './App.css';
+import User from './User'
+import Student from './Student'
 function App() {
+ 
+  function getData() {
+    alert("Hello from app component")
+  }
   return (
     <div className="App">
-      <Profile />
+     <User  data={getData} />
+     <Student data={getData} />
     </div>
   );
 }
 
 export default App;
+
+//User Component
+// function User(props) {
+    
+//     return(
+//         <div>
+//             <h1>User Component</h1>
+//             <button onClick={props.data}>Call Function</button>
+//         </div>
+//     )
+// }
+
+
+// export default User;
+//Student Component
+// function Student(props) {
+    
+//     return(
+//         <div>
+//             <h1>Student Component</h1>
+//             <button onClick={props.data}>Call Function</button>
+//         </div>
+//     )
+// }
+
+
+// export default Student;
