68ab9b890375afee00111a7c8463128360460855
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-27 12:47:19 +0530

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -11,3 +11,8 @@ function App() {
 }
 
 export default App;
+// In React, "hooks" are functions that allow you to use state and other React features in functional components.
+//   Prior to the introduction of hooks in React 16.8, stateful logic could only be used in class components. Hooks
+//   provide a more straightforward and concise way to work with state and other React features in functional components,
+//     promoting code reusability and making it easier to manage the component's state and lifecycle.
+
