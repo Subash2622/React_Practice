f08521bb687bb96992071b88feddb515162206be
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-27 12:44:05 +0530

    Update hooks.js

--- hooks.js
+++ hooks.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import React, { useState } from "react";
 function App() {
-  const [data, setData] = useState("Anil");
+  const [data, setData] = useState("Subash");
   return (
     <div className="App">
       <h1>Hooks in React {data}</h1>
-      <button onClick={() => setData("Peter")}>Update State</button>
+      <button onClick={() => setData("Shibu")}>Update State</button>
     </div>
   );
 }
