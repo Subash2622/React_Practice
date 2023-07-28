e63a06fec2419d9736a7607eeefea99eee4c30c2
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-28 12:16:14 +0530

    Update Array_listing_with_map_function.js

--- Array_listing_with_map_function.js
+++ Array_listing_with_map_function.js
@@ -3,16 +3,53 @@ import "./style.css";
 import style from "./custom.modoule.css";
 import { Button } from "react-bootstrap";
 function App() {
+  //const students = ["Subash", "Dinesh", "Saina", "Sanskruti"];
+  const studentDetails = [
+    {
+      name: "Subash",
+      contact: 9994,
+      email: "shubu@gmail.com",
+      age: 26,
+    },
+    {
+      name: "Dinesh",
+      contact: 8658,
+      email: "daku@gmail.com",
+      age: 26,
+    },
+    {
+      name: "Saina",
+      contact: 6370,
+      email: "saina@gmail.com",
+      age: 26,
+    },
+  ];
+  //Map function
+  // students.map((iteam) => {
+  //   console.log(iteam);
+  // });
+  // //For Loop
+  // for (let i = 0; i < students.length; i++) {
+  //   console.log("Hey I am ", students[i]);
+  // }
   return (
     <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
+      <h1>Handel Array with list</h1>
+      {/* {students.map((data) => (
+        <h1>I am a super star {data}</h1>
+      ))} */}
+      {studentDetails.map((data) => (
+        <table border="1">
+          <tr>
+            <td border="1"> {data.name} </td>
+            <td border="1">{data.contact}</td>
+            <td border="1"> {data.email}</td>
+            <td border="1">{data.age}</td>
+          </tr>
+        </table>
+      ))}
     </div>
   );
 }
-
+//In return statement for loop does not support, while map support in return statement
 export default App;
