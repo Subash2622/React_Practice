e3ad515654074c95666d801c84630d5d14ab1a85
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-28 12:57:50 +0530

    Update nested_list_with_nested_array.js

--- nested_list_with_nested_array.js
+++ nested_list_with_nested_array.js
@@ -1,16 +1,84 @@
 import "./App.css";
-import "./style.css";
-import style from "./custom.modoule.css";
-import { Button } from "react-bootstrap";
+import React from "react";
+import { Table } from "react-bootstrap";
 function App() {
+  const users = [
+    {
+      name: "Shubu",
+      email: "shubu@test.com",
+      address: [
+        { hm: "101", city: "Noida", country: "India" },
+        { hm: "10", city: "Gurgaon", country: "India" },
+        { hm: "23", city: "Noida", country: "India" },
+        { hm: "45", city: "Delhi", country: "India" },
+      ],
+    },
+    {
+      name: "Dinesh",
+      email: "Dinesh@test.com",
+      address: [
+        { hm: "101", city: "Noida", country: "India" },
+        { hm: "10", city: "Gurgaon", country: "India" },
+        { hm: "23", city: "Noida", country: "India" },
+        { hm: "45", city: "Delhi", country: "India" },
+      ],
+    },
+    {
+      name: "Saina",
+      email: "Saina@test.com",
+      address: [
+        { hm: "101", city: "Noida", country: "India" },
+        { hm: "10", city: "Gurgaon", country: "India" },
+        { hm: "23", city: "Noida", country: "India" },
+        { hm: "45", city: "Delhi", country: "India" },
+      ],
+    },
+    {
+      name: "Sanskruti",
+      email: "Sanskruti@test.com",
+      address: [
+        { hm: "101", city: "Noida", country: "India" },
+        { hm: "10", city: "Gurgaon", country: "India" },
+        { hm: "23", city: "Noida", country: "India" },
+        { hm: "45", city: "Delhi", country: "India" },
+      ],
+    },
+  ];
   return (
     <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
+      <h1>List with Nested Array</h1>
+      <Table variant="dark">
+        <tbody>
+          <tr>
+            <td>S.N</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Address</td>
+          </tr>
+          {users.map((item, i) => (
+            <tr key={i}>
+              <td>{i + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>
+                {/*  */}
+                <Table variant="dark">
+                  <tbody>
+                    {item.address.map((data) => (
+                      <tr>
+                        <td>{data.hm}</td>
+                        <td>{data.city}</td>
+                        <td>{data.country}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+                {/*  */}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
