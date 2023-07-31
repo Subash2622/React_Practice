fe5b3b275ccbc5662645e3a449493d42c0217289
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-31 12:21:28 +0530

    Update Fragment_in_reactJs.js

--- Fragment_in_reactJs.js
+++ Fragment_in_reactJs.js
@@ -1,18 +1,42 @@
-import "./App.css";
-import "./style.css";
-import style from "./custom.modoule.css";
-import { Button } from "react-bootstrap";
+import './App.css';
+import React from 'react'
+import Cols from './Cols'
 function App() {
   return (
-    <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
-    </div>
+    <>
+      <h1>React Fragment</h1>
+     <table>
+       <tbody>
+         <tr>
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+          <Cols />
+
+         </tr>
+       </tbody>
+     </table>
+      </>
+      
   );
 }
 
 export default App;
+
+//Anothe component 
+function Cols() {
+    return (
+        <>
+            <td>Subash</td>
+            <td>Sahoo</td>
+        </>
+    )
+}
+
+export default Cols;
