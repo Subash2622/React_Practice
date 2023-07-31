d544de70826623ab09e1a388c438d7fcc4c804c7
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-07-31 12:33:07 +0530

    Update Send_data_parent_to_child_component.js

--- Send_data_parent_to_child_component.js
+++ Send_data_parent_to_child_component.js
@@ -1,18 +1,31 @@
-import "./App.css";
-import "./style.css";
-import style from "./custom.modoule.css";
-import { Button } from "react-bootstrap";
+import React from 'react'
+import './App.css';
+import User from './User'
 function App() {
+  function getName(name)
+  {
+    alert(name)
+  }
   return (
     <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
+      <h1>Lifting State Up</h1>
+      <User getData={getName} />
     </div>
   );
 }
 
 export default App;
+
+//New Component- User
+function User(props)
+{
+    const name="Anil Sidhu"
+    return(
+        <div>
+            <h1>User Name is : </h1>
+            <button onClick={()=>props.getData(name)} >Click Me</button>
+        </div>
+    )
+}
+
+export default User;
