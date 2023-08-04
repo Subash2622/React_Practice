82fda64512eae5d4865cf6200024b1a598de6574
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-08-04 13:24:17 +0530

    Update controlledComponent.js

--- controlledComponent.js
+++ controlledComponent.js
@@ -1,16 +1,31 @@
 //When we controll input field with state
 import './App.css';
-import React,{useState} from 'react'
+import React, { useRef } from 'react'
 function App() {
-  const [val,setVal]=useState("")
+  let inputRef = useRef(null)
+  let inputRef2 = useRef(null)
 
-   return (
+  function submitForm(e) {
+    e.preventDefault()
+    console.warn("input field 1 value : ", inputRef.current.value)
+    console.warn("input field 2 value : ", inputRef2.current.value)
+    let input3 = document.getElementById('input3').value
+    console.warn("input field 3 value : ", input3)
+
+
+  }
+  return (
     <div className="App">
-      <h1>Controlled Component </h1>
-      <input type="text" value={val} onChange={(e)=>setVal(e.target.value)} />
-   <h1>{val}</h1>
+      <h1>Uncontrolled Component </h1>
+      <form onSubmit={submitForm} >
+        <input ref={inputRef} type="text" /> <br /> <br />
+        <input ref={inputRef2} type="text" /> <br /> <br />
+        <input id="input3" type="text" /> <br /> <br />
+
+        <button>Submit</button>
+      </form>
     </div>
   );
 
 }
-export default  App;
+export default App;
