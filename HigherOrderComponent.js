03f0b5db31fe5e5745d0d3c2b5df24f660d4bf40
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-08-04 13:35:13 +0530

    Update HigherOrderComponent.js

--- HigherOrderComponent.js
+++ HigherOrderComponent.js
@@ -1,18 +1,35 @@
-import "./App.css";
-import "./style.css";
-import style from "./custom.modoule.css";
-import { Button } from "react-bootstrap";
+import './App.css';
+import React, { useRef, useState } from 'react'
 function App() {
   return (
     <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
+      <h1>HOC </h1>
+      <HOCRed cmp={Counter} />
+      <HOCGreen cmp={Counter} />
+      <HOCBlue cmp={Counter} />
+
     </div>
   );
+}
+function HOCRed(props)
+{
+  return <h2 style={{backgroundColor:'red',width:100}}>Red<props.cmp /></h2>
+}
+function HOCGreen(props)
+{
+  return <h2 style={{backgroundColor:'green',width:100}}>Grren<props.cmp /></h2>
+}
+function HOCBlue(props)
+{
+  return <h2 style={{backgroundColor:'blue',width:100}}>blue <props.cmp /></h2>
+}
+function Counter()
+{
+  const [count,setCount]=useState(0)
+  return<div>
+    <h3>{count}</h3>
+    <button onClick={()=>setCount(count+1)}>Update</button>
+  </div>
 }
 
 export default App;
