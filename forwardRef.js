7198cf29090a3d98314612264b4464c6dc1f303d
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-08-04 13:12:43 +0530

    Update forwardRef.js

--- forwardRef.js
+++ forwardRef.js
@@ -1,18 +1,36 @@
-import "./App.css";
-import "./style.css";
-import style from "./custom.modoule.css";
-import { Button } from "react-bootstrap";
+import './App.css';
+import React ,{useRef} from 'react'
+import User from './User'
 function App() {
+  let inputRef=useRef(null)
+  function updateInput()
+  {
+    inputRef.current.value="1000";
+    inputRef.current.style.color="red"
+    inputRef.current.focus()
+
+
+  }
   return (
     <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
+      <h1>forwardRef in React </h1>
+      <User ref={inputRef} />
+      <button onClick={updateInput} >Update Input Box</button>
     </div>
   );
+
+}
+export default App;
+
+//New Component
+import React,{forwardRef} from 'react'
+function User(props,ref)
+{
+    return(
+        <div>
+            <input ref={ref} type="text" />
+        </div>
+    )
 }
 
-export default App;
+export default forwardRef(User);
