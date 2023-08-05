7a7b250dfdfa790842899cf37cc569ce3e03ab6d
Subash Chandra Sahoo <104317974+Subash2622@users.noreply.github.com>
2023-08-05 14:56:01 +0530

    Update reactRouterDom_nabar-anchortag-Links.js

--- reactRouterDom_nabar-anchortag-Links.js
+++ reactRouterDom_nabar-anchortag-Links.js
@@ -1,16 +1,19 @@
 import "./App.css";
-import "./style.css";
-import style from "./custom.modoule.css";
-import { Button } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
+import Navbar from "./component/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1 className="primary">Hello World 1</h1>
-      <h1 style={{ color: "red", backgroundColor: "yellow" }}>
-        Hello World 2{" "}
-      </h1>
-      <h1 className={style.success}>Hello World 3</h1>
-      <Button>Click Hello</Button>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
